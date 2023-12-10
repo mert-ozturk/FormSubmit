630f5b306ed19679d857162ffec07ab584f628bb
mert-ozturk <mertozturk.seo.yonetim@gmail.com>
2023-12-10 12:38:24 +0300

    islem2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react'
-import { useFormik,Formik,Form,Field } from 'formik'
+import { useFormik } from 'formik'
 import { signupValidation } from './signupValuedation'
 
 
 const App = () => {
-
-
+  
 const initialValues = {
   name:'',
   email:'',
@@ -13,43 +12,51 @@ const initialValues = {
   cpassword:'',
 }
 
+ const {values,handleBlur,handleChange,handleSubmit,errors} = useFormik({
+  initialValues: initialValues,
+  validationSchema: signupValidation,
+  onSubmit:(values) => {
+    console.log(values)
+  }
+})
+
   return (
  
-    <div className='App'>
-      <Formik
-      initialValues={initialValues}
-      validationSchema={signupValidation}
-      >
-        {({errors})=> (
-             <Form >
-             <label htmlFor='name'>Name</label>
-             <br/>
-             <Field type="text" name="name"></Field>
-             <br/>
-             {errors.name && <small>{errors.name}</small>}
-             <br/>
-             <label htmlFor='email'>Email</label>
-             <br/>
-             <Field type="email" name="email"></Field>
-             <br/>
-             {errors.email && <small>{errors.email}</small>}
-             <br/>
-             <label htmlFor='password'>Password</label>
-             <br/>
-             <Field type="password" name="password"></Field>
-             <br/>
-             {errors.password && <small>{errors.password}</small>}
-             <br/>
-             <label htmlFor='cpassword'>Tekar Password</label>
-             <br/>
-             <Field type="password" name="cpassword"></Field>
-             <br/>
-             {errors.cpassword && <small>{errors.cpassword}</small>}
-             <br/>
-             <button type='submit'>Submit</button>
-           </Form>
-        )}
-      </Formik>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor='name'>Name</label>
+        <br/>
+        <input type='text' name='name' value={values.name}
+        onBlur={handleBlur} onChange={handleChange}/>
+        <br/>
+        {errors.name && <small>{errors.name}</small>}
+        <br/>
+        <label htmlFor='email'>Email</label>
+        <br/>
+        <input type='email' name='email'
+        value={values.email}
+        onBlur={handleBlur} onChange={handleChange}/>
+        <br/>
+        {errors.email && <small>{errors.email}</small>}
+        <br/>
+        <label htmlFor='password'>Password</label>
+        <br/>
+        <input type='password' name='password'value={values.password}
+        onBlur={handleBlur} onChange={handleChange}
+        />
+        <br/>
+        {errors.password && <small>{errors.password}</small>}
+        <br/>
+        <label htmlFor='cpassword'>Tekar Password</label>
+        <br/>
+        <input type='cpassword' name='cpassword'value={values.cpassword}
+        onBlur={handleBlur} onChange={handleChange}
+        />
+        <br/>
+        {errors.cpassword && <small>{errors.cpassword}</small>}
+        <br/>
+        <button type='submit'>Submit</button>
+      </form>
     </div>
   )
 }
